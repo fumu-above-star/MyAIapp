2a765c961fd129490054d81fb67206244d626e22
fumu-above-star <iino@ns.cs.ehime-u.ac.jp>
2023-11-26 09:55:15 +0900

    base.html -> layout.html

--- app.py
+++ app.py
@@ -4,33 +4,14 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET":
-        posts = Post.query.order_by(Post.due).all()  # (締め切り順に)
-        return render_template("index.html", posts=posts)
-
-    else:
-        title = request.form.get("title")
-        detail = request.form.get("detail")
-        due = request.form.get("due")
-
-        due = datetime.strptime(due, "%Y-%m-%d")
-        new_post = Post(title=title, detail=detail, due=due)
-
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect("/")
+    return render_template("index.html")
 
 
 @app.route("/test")
 def test():
     return render_template("test.html")
-
-
-@app.route("/create")
-def create():
-    return render_template("create.html")
 
 
 if __name__ == "__main__":
